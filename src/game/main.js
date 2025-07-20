37ca32db4d658dd403c1ab9dcd6204ef825d8b4d
cagecorn <lmcbv@naver.com>
2025-07-21 02:51:43 +0900

    Revert "Apply pixel-perfect settings"

--- src/game/main.js
+++ src/game/main.js
@@ -24,13 +24,13 @@ const config = {
     backgroundColor: 'transparent', // 배경색을 투명하게 만듭니다.
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        // 정수 배율을 사용하여 픽셀 아트 표현을 개선합니다.
-        zoom: Math.floor(window.devicePixelRatio)
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
     render: {
-        pixelArt: true,
-        antialias: false
+        pixelArt: false,
+        antialias: true, // 안티에일리어싱을 활성화하여 이미지를 부드럽게 표현합니다.
+        resolution: window.devicePixelRatio || 1, // 기기의 픽셀 비율에 맞춰 해상도를 설정합니다.
+        roundPixels: true,
     },
     // Boot 씬만 초기 설정에 등록합니다.
     // Boot 씬이 실행되면서 나머지 씬들을 동적으로 추가합니다.
